eb565b636f1bdbe3131c7269aa0b7dc94833889e
emanuel.developer82 <emanuel.developer82@gmail.com>
2022-01-28 13:13:06 -0300

    Melhorias da aplicação;

---
+++ src/app/contas/contas-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ContasComponent } from './contas/contas.component';
+
+const routes: Routes = [
+  { path: '', component: ContasComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ContasRoutingModule { }
