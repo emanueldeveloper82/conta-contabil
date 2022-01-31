e35a9fd3180c82b0a4dc940d781de2e428e70549
emanuel.developer82 <emanuel.developer82@gmail.com>
2022-01-31 10:50:31 -0300

    Ajustes no arquivo de constantes;

--- constantes.ts
+++ constantes.ts
@@ -2,3 +2,15 @@ export class Constantes {
     static readonly LOCAL_DATE = 'DD/MM/YYYY';
     static readonly LOCAL_DATE_TIME = 'DD/MM/YYYY hh:mm:ss';
 }
+
+export const MY_FORMATS = {
+    parse: {
+      dateInput: 'LL',
+    },
+    display: {
+      dateInput: Constantes.LOCAL_DATE,
+      monthYearLabel: 'YYYY',
+      dateA11yLabel: 'LL',
+      monthYearA11yLabel: 'YYYY',
+    },
+  };
